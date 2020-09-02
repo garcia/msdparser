7173cff28f0835a9fcbbd77f5a3ef09a54c1c4c4
Ash Garcia <github@garcia.sh>
2020-09-01 21:37:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='msdparser',
-      version='0.1',
+      version='0.1.1',
       description='Simple MSD parser (rhythm game format)',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       keywords='stepmania simfile sm ssc dwi',
       url='http://github.com/garcia/msdparser',
       author='Ash Garcia',
       author_email='msdparser@garcia.sh',
       license='MIT',
       packages=['msdparser'],
+      python_requires='>=3.6',
       zip_safe=True)
