e60d1878e605ca03b7853f23ecfa0db3e191b8ad
Ash Garcia <github@garcia.sh>
2020-09-13 22:09:39 -0700

    Move version to __init__

--- msdparser/__init__.py
+++ msdparser/__init__.py
@@ -1,1 +1,3 @@
 from .msdparser import MSDParser
+
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
 from setuptools import setup
 
+from msdparser import __version__ as version
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='msdparser',
-      version='0.1.3',
-      description='Simple MSD parser (rhythm game format)',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Ash Garcia',
-      author_email='python-msdparser@garcia.sh',
-      url='http://github.com/garcia/msdparser',
-      packages=['msdparser'],
-      classifiers=[
+setup(
+    name='msdparser',
+    version=version,
+    description='Simple MSD parser (rhythm game format)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ash Garcia',
+    author_email='python-msdparser@garcia.sh',
+    url='http://github.com/garcia/msdparser',
+    packages=['msdparser'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Topic :: Games/Entertainment',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Typing :: Typed',
-      ],
-      license='MIT',
-      keywords='stepmania simfile sm ssc dwi',
-      python_requires='>=3.6',
-      zip_safe=False,
+    ],
+    license='MIT',
+    keywords='stepmania simfile sm ssc dwi',
+    python_requires='>=3.6',
+    zip_safe=False,
+    command_options={
+        'build_sphinx': {
+            'version': ('setup.py', version),
+        },
+    },
 )
