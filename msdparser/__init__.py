fcb2f078690b626d4b560c3758280841ce561dd9
Ash Garcia <github@garcia.sh>
2021-06-08 18:09:47 -0700

    function-ify MSDParser into parse_msd

--- msdparser/__init__.py
+++ msdparser/__init__.py
@@ -1,3 +1,153 @@
-from .msdparser import MSDParser
-
 __version__ = '1.0.0-alpha.3'
+
+import enum
+from io import StringIO
+from typing import Iterable, Iterator, Optional, TextIO, Tuple, Union
+
+
+__all__ = ['parse_msd', 'MSDParserError']
+
+
+def trailing_newline(line: str):
+    end_of_line = len(line.rstrip('\r\n'))
+    return line[end_of_line:]
+
+
+class State(enum.Enum):
+    """
+    Encapsulates the high-level state of the MSD parser.
+    """
+    SEEK = enum.auto() # Discard until a '#' is reached
+    KEY = enum.auto() # Read the key until a ':' or ';' is reached
+    VALUE = enum.auto() # Read the value until a ';' is reached
+
+
+class MSDParserError(Exception):
+    """
+    Raised when a non-whitespace character is found outside parameters.
+
+    The byte order mark (BOM, U+FEFF) is special-cased as whitespace.
+    """
+
+
+class ParameterState(object):
+    """
+    Encapsulates the complete state of the MSD parser, including the partial
+    key/value pair.
+    """
+    __slots__ = ['key', 'value', 'state', 'ignore_stray_text']
+
+    def __init__(self, ignore_stray_text):
+        self.ignore_stray_text = ignore_stray_text
+        self._reset()
+    
+    def _reset(self) -> None:
+        """
+        Clear the key & value and set the state to SEEK.
+        """
+        self.key = StringIO()
+        self.value = StringIO()
+        self.state = State.SEEK
+    
+    def write(self, text) -> None:
+        """
+        Write to the key or value, or handle stray text if seeking.
+        """
+        if self.state is State.KEY:
+            self.key.write(text)
+        elif self.state is State.VALUE:
+            self.value.write(text)
+        elif not self.ignore_stray_text:
+            if text and not text.isspace() and text != '\ufeff':
+                raise MSDParserError(f"stray {repr(text)} encountered")
+    
+    def complete(self) -> Tuple[str, str]:
+        """
+        Return the parameter as a (key, value) tuple and reset to the initial
+        key / value / state.
+        """
+        parameter = (self.key.getvalue(), self.value.getvalue())
+        self._reset()
+        return parameter
+
+
+def parse_msd(
+    *,
+    file: Optional[Union[TextIO, Iterator[str]]] = None,
+    string: Optional[str] = None,
+    ignore_stray_text: bool = False
+) -> Iterator[Tuple[str, str]]:
+    """
+    Simple parser for MSD files.
+
+    :param Optional[Union[TextIO,Iterator[str]]] file:
+        file or file-like object to read (mutually excludes `string`)
+    :param Optional[str] string:
+        string to read (mutually excludes `file`)
+    :param bool ignore_stray_text:
+        whether to suppress :class:`MSDParserError` when stray
+        non-whitespace text is found outside parameters
+    """
+    file_or_string = file or string
+    if file_or_string is None:
+        raise ValueError('must provide either a file or a string')
+    if file is not None and string is not None:
+        raise ValueError('must provide either a file or a string, not both')
+    
+    # We've proven that `file_or_string` is not None because mypy can't prove
+    # "either `file` or `string` is not None", hence the awkwardness here.
+    # IMO this tradeoff is worth it for the explicitness of separate, named
+    # parameters; otherwise users might try to pass filenames as input strings.
+    line_iterator: Iterable[str]
+    if isinstance(file_or_string, str):
+        line_iterator = file_or_string.splitlines(True)
+    else:
+        line_iterator = file_or_string
+
+    ps = ParameterState(ignore_stray_text=ignore_stray_text)
+
+    for line in line_iterator:
+
+        # Handle missing ';' outside of the loop
+        if ps.state is not State.SEEK and line.startswith('#'):
+            yield ps.complete()
+
+        for col, char in enumerate(line):
+
+            # Read normal characters at the start of the loop for speed
+            if char not in ':;/#':
+                ps.write(char)
+            
+            elif char == '#':
+                # Start of the next parameter
+                if ps.state is State.SEEK:
+                    ps.state = State.KEY
+                # Treat '#' normally elsewhere
+                else:
+                    ps.write(char)
+
+            elif char == '/':
+                # Skip the rest of the line for comments
+                if col+1 < len(line) and line[col+1] == '/':
+                    # Preserve the newline
+                    ps.write(trailing_newline(line))
+                    break
+                # Write the '/' if it's not part of a comment
+                ps.write(char)
+
+            elif char == ';':
+                # End of the parameter
+                if ps.state is not State.SEEK:
+                    yield ps.complete()
+            
+            elif char == ':':
+                # Key-value separator
+                if ps.state is State.KEY:
+                    ps.state = State.VALUE
+                # Treat ':' normally elsewhere
+                else:
+                    ps.write(char)
+
+    # Handle missing ';' at the end of the input
+    if ps.state is not State.SEEK:
+        yield ps.complete()
---
+++ msdparser/tests/test_module.py
@@ -0,0 +1,101 @@
+import unittest
+from io import StringIO
+
+from msdparser import MSDParserError, parse_msd
+
+class TestMSDParser(unittest.TestCase):
+    
+    def test_constructor(self):
+        data = "#A:B;"
+        from_file = parse_msd(file=StringIO(data))
+        from_string = parse_msd(string=data)
+        
+        self.assertEqual(next(from_file), next(from_string))
+        self.assertRaises(StopIteration, next, from_file)
+        self.assertRaises(StopIteration, next, from_string)
+    
+    def test_empty(self):
+        parse = parse_msd(string="")
+        
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_normal_characters(self):
+        parse = parse_msd(string='#A1,./\'"[]\{\}|`~!@#$%^&*()-_=+ \r\n\t:A1,./\'"[]\{\}|`~!@#$%^&*()-_=+ \r\n\t:;')
+        key, value = next(parse)
+
+        self.assertEqual('A1,./\'"[]\{\}|`~!@#$%^&*()-_=+ \r\n\t', key)
+        self.assertEqual('A1,./\'"[]\{\}|`~!@#$%^&*()-_=+ \r\n\t:', value)
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_comments(self):
+        parse = parse_msd(string='#A// comment //\r\nBC:D// ; \nEF;//#NO:PE;')
+        
+        self.assertEqual(('A\r\nBC', 'D\nEF'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_comment_with_no_newline_at_eof(self):
+        parse = parse_msd(string='#ABC:DEF// eof')
+
+        self.assertEqual(('ABC', 'DEF'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+
+    def test_empty_key(self):
+        parse = parse_msd(string='#:ABC;#:DEF;')
+
+        self.assertEqual(('', 'ABC'), next(parse))
+        self.assertEqual(('', 'DEF'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_empty_value(self):
+        parse = parse_msd(string='#ABC:;#DEF:;')
+
+        self.assertEqual(('ABC', ''), next(parse))
+        self.assertEqual(('DEF', ''), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+
+    def test_missing_value(self):
+        parse = parse_msd(string='#ABC;#DEF;')
+
+        self.assertEqual(('ABC', ''), next(parse))
+        self.assertEqual(('DEF', ''), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+
+    def test_missing_semicolon(self):
+        parse = parse_msd(string='#A:B\nCD;#E:FGH\n#IJKL// comment\n#M:NOP')
+        
+        self.assertEqual(('A', 'B\nCD'), next(parse))
+        self.assertEqual(('E', 'FGH\n'), next(parse))
+        self.assertEqual(('IJKL\n', ''), next(parse))
+        self.assertEqual(('M', 'NOP'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_missing_value_and_semicolon(self):
+        parse = parse_msd(string='#A\n#B\n#C\n')
+
+        self.assertEqual(('A\n', ''), next(parse))
+        self.assertEqual(('B\n', ''), next(parse))
+        self.assertEqual(('C\n', ''), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_unicode(self):
+        parse = parse_msd(string='#TITLE:実例;\n#ARTIST:楽士;')
+        
+        self.assertEqual(('TITLE', '実例'), next(parse))
+        self.assertEqual(('ARTIST', '楽士'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+    
+    def test_stray_text(self):
+        parse = parse_msd(string='#A:B;n#C:D;')
+
+        self.assertEqual(('A', 'B'), next(parse))
+        self.assertRaises(MSDParserError, next, parse)
+    
+    def test_stray_text_with_ignore_stray_text(self):
+        parse = parse_msd(string='#A:B;n#C:D;', ignore_stray_text=True)
+
+        self.assertEqual(('A', 'B'), next(parse))
+        self.assertEqual(('C', 'D'), next(parse))
+        self.assertRaises(StopIteration, next, parse)
+
+if __name__ == '__main__':
+    unittest.main()
