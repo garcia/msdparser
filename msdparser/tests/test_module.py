6fef223a602ff4992ca4720c520d132ec653952d
Ash Garcia <github@garcia.sh>
2021-12-31 14:09:08 -0800

    tests

--- msdparser/tests/test_module.py
+++ msdparser/tests/test_module.py
@@ -1,9 +1,55 @@
+from typing import Tuple
 import unittest
 from io import StringIO
 
-from msdparser import MSDParserError, parse_msd
+from msdparser import MSDParameter, MSDParserError, parse_msd
 
-class TestMSDParser(unittest.TestCase):
+
+class TestMSDParameter(unittest.TestCase):
+    def test_constructor(self):
+        param = MSDParameter('key', 'value')
+        
+        self.assertEqual('key', param.key)
+        self.assertEqual('value', param.value)
+        self.assertIs(param[0], param.key)
+        self.assertIs(param[1], param.value)
+
+    def test_backwards_compatible_type(self):
+        # Pylance test: replacing (k, v) with MSDParameter(k, v) shouldn't
+        # cause any type errors in client code
+        _: Tuple[str, str] = MSDParameter('key', 'value')
+    
+    def test_str_with_escapes(self):
+        param = MSDParameter('key', 'value')
+        evil_param = MSDParameter('ABC:DEF;GHI//JKL\\MNO', 'abc:def;ghi//jkl\\mno')
+
+        self.assertEqual('#key:value;', str(param))
+        self.assertEqual('#ABC\\:DEF\\;GHI\\//JKL\\\\MNO:abc:def\\;ghi\\//jkl\\\\mno;', str(evil_param))
+    
+    def test_str_without_escapes(self):
+        param = MSDParameter('key', 'value')
+        multi_value_param = MSDParameter('key', 'abc:def')
+        param_with_literal_backslashes = MSDParameter('ABC\\DEF', 'abc\\def')
+        invalid_params = (
+            # `:` separator in key
+            MSDParameter('ABC:DEF', 'abcdef'),
+            # `;` terminator in key or value
+            MSDParameter('ABC;DEF', 'abcdef'),
+            MSDParameter('ABCDEF', 'abc;def'),
+            # `//` comment initializer in key or value
+            MSDParameter('ABC//DEF', 'abcdef'),
+            MSDParameter('ABCDEF', 'abc//def'),
+        )
+
+        self.assertEqual('#key:value;', param.__str__(escapes=False))
+        self.assertEqual('#key:abc:def;', multi_value_param.__str__(escapes=False))
+        self.assertEqual('#ABC\\DEF:abc\\def;', param_with_literal_backslashes.__str__(escapes=False))
+
+        for invalid_param in invalid_params:
+            self.assertRaises(ValueError, invalid_param.__str__, escapes=False)
+
+
+class TestParseMSD(unittest.TestCase):
     
     def test_constructor(self):
         data = "#A:B;"
@@ -103,7 +149,7 @@ class TestMSDParser(unittest.TestCase):
         self.assertEqual(('C', 'D'), next(parse))
         self.assertRaises(StopIteration, next, parse)
     
-    def test_escaped_characters(self):
+    def test_escapes(self):
         parse = parse_msd(string='#A\\:B:C\\;D;#E\\#F:G\\\\H;#LF:\\\nLF;')
 
         self.assertEqual(('A:B', 'C;D'), next(parse))
