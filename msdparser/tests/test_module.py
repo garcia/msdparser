f5104f9f6b27bdc7f6b1d90fcb7b56d1a6c011fc
Ash Garcia <github@garcia.sh>
2022-04-04 21:35:12 -0700

    tests

--- msdparser/tests/test_module.py
+++ msdparser/tests/test_module.py
@@ -191,6 +191,23 @@ class TestLexMSD(unittest.TestCase):
         self.assertEqual((MSDToken.NEXT_COMPONENT, ':'), next(lexer))
         self.assertEqual((MSDToken.TEXT, 'MNO\nPQR# STU'), next(lexer))
         self.assertRaises(StopIteration, next, lexer)
+    
+    def test_tokens_without_escapes(self):
+        lexer = lex_msd(string='#ABC:DEF\\:GHI;\n#JKL:MNO\nPQR# STU', escapes=False)
+
+        self.assertEqual((MSDToken.START_PARAMETER, '#'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, 'ABC'), next(lexer))
+        self.assertEqual((MSDToken.NEXT_COMPONENT, ':'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, 'DEF\\'), next(lexer))
+        self.assertEqual((MSDToken.NEXT_COMPONENT, ':'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, 'GHI'), next(lexer))
+        self.assertEqual((MSDToken.END_PARAMETER, ';'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, '\n'), next(lexer))
+        self.assertEqual((MSDToken.START_PARAMETER, '#'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, 'JKL'), next(lexer))
+        self.assertEqual((MSDToken.NEXT_COMPONENT, ':'), next(lexer))
+        self.assertEqual((MSDToken.TEXT, 'MNO\nPQR# STU'), next(lexer))
+        self.assertRaises(StopIteration, next, lexer)
 
 
 if __name__ == '__main__':
