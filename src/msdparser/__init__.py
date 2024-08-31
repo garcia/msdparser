e9ba7e012edbe5d2ee9f07a4cf9288b3e562ad67
Ash Garcia <github@garcia.sh>
2024-08-30 20:58:20 -0700

    update version

--- src/msdparser/__init__.py
+++ src/msdparser/__init__.py
@@ -2,7 +2,8 @@
 This top-level module re-exports :func:`.parse_msd`, :class:`.MSDParameter`,
 and :class:`.MSDParserError` for convenience.
 """
-__version__ = "2.1.0rc1"
+
+__version__ = "3.0.0a1"
 __all__ = ["MSDParserError", "MSDParameter", "parse_msd"]
 
 from .parser import MSDParserError, parse_msd
